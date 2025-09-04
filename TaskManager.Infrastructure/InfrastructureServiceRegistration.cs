e6ebd9755e299b107ccb2c4af394adcc6f50babb
Smaga Max <smaga.max@gmail.com>
2025-09-04 15:19:45 +0300

    implement IPasswordHasher in infrastructure layer

---
+++ TaskManager.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TaskManager.Application.Contracts.Infrastructure;
+
+namespace TaskManager.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddTransient<IPasswordHasher, PasswordHasher>();
+
+            return services;
+        }
+    }
+}
---
+++ TaskManager.Infrastructure/PasswordHasher.cs
@@ -0,0 +1,17 @@
+﻿using TaskManager.Application.Contracts.Infrastructure;
+
+namespace TaskManager.Infrastructure
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string Hash(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool Verify(string password, string hash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hash);
+        }
+    }
+}
