2af85f795927437259f9debd3dde2333179963ac
Smaga Max <smaga.max@gmail.com>
2025-09-04 15:14:13 +0300

    configure identity layer and implement IAuthenticationService

---
+++ TaskManager.Application/Contracts/Persistance/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using TaskManager.Domain.Entities;
+
+namespace TaskManager.Application.Contracts.Persistance
+{
+    public interface IUserRepository : IAsyncRepository<User>
+    {
+        Task<User?> GetByEmailAsync(string email);
+        Task<User?> GetByUsernameAsync(string username);
+    }
+}
---
+++ TaskManager.Identity/Services/AuthenticationService.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using TaskManager.Application.Contracts.Identity;
+using TaskManager.Application.Contracts.Infrastructure;
+using TaskManager.Application.Contracts.Persistance;
+using TaskManager.Application.DTOs;
+using TaskManager.Domain.Entities;
+
+namespace TaskManager.Identity.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly JwtSettings _jwtSettings;
+        private readonly IUserRepository _userRepository;
+        private readonly IPasswordHasher _passwordHasher;
+        public AuthenticationService(
+            IUserRepository userRepository,
+            IPasswordHasher passwordHasher,
+            IOptions<JwtSettings> jwtSettings)
+        {
+            _jwtSettings = jwtSettings.Value;
+            _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
+        }
+
+        public async Task<string> RegisterAsync(RegistrationRequest request)
+        {
+            var existingEmail = await _userRepository.GetByEmailAsync(request.Email);
+            if (existingEmail != null)
+                throw new Exception("Email already exists");
+
+            var existingUsername = await _userRepository.GetByUsernameAsync(request.Username);
+            if (existingUsername != null)
+                throw new Exception("Username already exists");
+
+            var hashedPassword = _passwordHasher.Hash(request.Password);
+
+            var user = new User
+            {
+                Username = request.Username,
+                Email = request.Email,
+                PasswordHash = hashedPassword,
+            };
+
+            await _userRepository.AddAsync(user);
+
+            return user.Id.ToString();
+        }
+
+        public async Task<AuthenticationResponse> AuthenticateAsync(AuthenticationRequest request)
+        {
+            var user = await _userRepository.GetByEmailAsync(request.Email);
+            if (user == null)
+                throw new Exception("Invalid credentials");
+
+            if (!_passwordHasher.Verify(request.Password, user.PasswordHash))
+                throw new Exception("Invalid credentials");
+
+            var token = GenerateToken(user);
+
+            return new AuthenticationResponse
+            {
+                Id = user.Id,
+                Username = user.Username,
+                Email = user.Email,
+                Token = token
+            };
+        }
+        private string GenerateToken(User user)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.UniqueName, user.Username)
+            };
+
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Key));
+            var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+            var jwtSecurityToken = new JwtSecurityToken(
+                issuer: _jwtSettings.Issuer,
+                audience: _jwtSettings.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(_jwtSettings.DurationInMinutes),
+                signingCredentials: signingCredentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+        }
+    }
+}
