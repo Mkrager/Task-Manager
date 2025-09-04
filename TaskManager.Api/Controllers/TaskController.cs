ac592f09019bdcde022ed62d3658afa5810faf41
Smaga Max <smaga.max@gmail.com>
2025-09-04 21:49:24 +0300

    implement GetTaskById endpoint

--- TaskManager.Api/Controllers/TaskController.cs
+++ TaskManager.Api/Controllers/TaskController.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TaskManager.Application.Contracts;
 using TaskManager.Application.Features.Tasks.Commads.CreateTask;
+using TaskManager.Application.Features.Tasks.Commads.DeleteTask;
+using TaskManager.Application.Features.Tasks.Commads.UpdateTask;
+using TaskManager.Application.Features.Tasks.Queries.GetTaskDetails;
 using TaskManager.Application.Features.Tasks.Queries.GetUserTasksList;
 
 namespace TaskManager.Api.Controllers
@@ -32,5 +35,37 @@ namespace TaskManager.Api.Controllers
             return Ok(id);
         }
 
+        [Authorize]
+        [HttpPut(Name = "UpdateTask")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Update([FromBody] UpdateTaskCommand updateTaskCommand)
+        {
+            await mediator.Send(updateTaskCommand);
+            return NoContent();
+        }
+
+        [Authorize]
+        [HttpDelete("{id}", Name = "DeleteTask")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            var deleteTaskCommand = new DeleteTaskCommand() { Id = id };
+            await mediator.Send(deleteTaskCommand);
+            return NoContent();
+        }
+
+        [Authorize]
+        [HttpGet("{id}", Name = "GetTaskById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<TaskDetailVm>> GetTaskById(Guid id)
+        {
+            var getTaskDetailQuery = new GetTaskDetailQuery() { Id = id };
+            return Ok(await mediator.Send(getTaskDetailQuery));
+        }
     }
 }
